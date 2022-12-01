ae5344cf73e7999b555a37204412a3d634b793b7
jeef2212 <51117842+jeef2212@users.noreply.github.com>
2022-12-01 16:29:31 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Header from "../src/Header"
+import Meme from "../src/Meme"
+
+export default function App() {
+    return (
+        <div>
+            <Header />
+            <Meme />
+        </div>
+    )
+}
